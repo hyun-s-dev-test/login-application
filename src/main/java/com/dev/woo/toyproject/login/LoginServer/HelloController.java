1780021d730bae0547aed9b5b4e37305e41ddb1e
0woodev <0woodev@gmail.com>
2022-03-14 20:23:46 +0900

    feat: github actions 작성 및 간단한 hello controller 생성

--- src/main/java/com/dev/woo/toyproject/login/LoginServer/HelloController.java
+++ src/main/java/com/dev/woo/toyproject/login/LoginServer/HelloController.java
@@ -1,4 +1,13 @@
 package com.dev.woo.toyproject.login.LoginServer;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello world!";
+    }
 }
