832a2faea2d8b179aafa6bdf91eeb29816ed2176
0woodev <0woodev@gmail.com>
2022-03-16 08:18:08 +0900

    feat: User domain 생성

--- src/main/java/com/dev/woo/toyproject/login/loginserver/LoginServerApplication.java
+++ src/main/java/com/dev/woo/toyproject/login/loginserver/LoginServerApplication.java
@@ -2,8 +2,10 @@ package com.dev.woo.toyproject.login.loginserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableJpaAuditing
 @SpringBootApplication // (exclude = {DataSourceAutoConfiguration.class})
 public class LoginServerApplication {
 	public static void main(String[] args) {
