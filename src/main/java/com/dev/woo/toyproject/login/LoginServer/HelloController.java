8857b7b3ab081229699cdcd717a6b13fe82a45d1
0woodev <0woodev@gmail.com>
2022-03-15 10:26:48 +0900

    test: dev 브랜치 CICD 테스트

--- src/main/java/com/dev/woo/toyproject/login/LoginServer/HelloController.java
+++ src/main/java/com/dev/woo/toyproject/login/LoginServer/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Main Branch CI&CD Test\n" +
+        return "Dev Branch CI&CD Test\n" +
                 "Hello world!";
     }
 }
