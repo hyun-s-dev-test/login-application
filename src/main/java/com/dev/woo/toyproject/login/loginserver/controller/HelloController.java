87951ab6237e211e02126586386db22c2fb5c5af
0woodev <0woodev@gmail.com>
2022-03-16 08:17:53 +0900

    feat: User domain 생성

--- src/main/java/com/dev/woo/toyproject/login/loginserver/controller/HelloController.java
+++ src/main/java/com/dev/woo/toyproject/login/loginserver/controller/HelloController.java
@@ -1,26 +1,14 @@
-package com.dev.woo.toyproject.login.loginserver.controller;
+package com.dev.woo.toyproject.login.LoginServer.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "Hello Controller", tags = "Hello")
 @RestController
 public class HelloController {
 
-    @ApiOperation(value = "모든 유저 조회", notes = "모든 유저의 정보를 가져옵니다.(비밀번호, 토큰 제외)")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK!"),
-            @ApiResponse(code = 400, message = "BAD REQUEST!"),
-            @ApiResponse(code = 404, message = "NOT FOUND!"),
-            @ApiResponse(code = 500, message = "INTERNAL SERVER ERROR!"),
-    })
     @GetMapping("/")
     public String hello() {
         return "Dev Branch CI&CD Test\n" +
-                "Hello world! MYSQL 연결까지 끝났다.!";
+                "Hello world! MYSQL connect finished!";
     }
 }
---
+++ src/main/java/com/dev/woo/toyproject/login/loginserver/domain/user/User.java
@@ -0,0 +1,64 @@
+package com.dev.woo.toyproject.login.loginserver.domain.user;
+
+import com.dev.woo.toyproject.login.loginserver.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pk;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String gender;
+
+    @Column
+    private LocalDate birth;
+
+    @Column
+    private String socialType;
+
+    @Column
+    private String token;
+
+    @Column
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String id, String password, String gender, String socialType, LocalDate birth, String token, Role role, String phone) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+        this.gender = gender;
+        this.socialType = socialType;
+        this.token = token;
+        this.birth = birth;
+        this.role = role;
+        this.phone = phone;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
