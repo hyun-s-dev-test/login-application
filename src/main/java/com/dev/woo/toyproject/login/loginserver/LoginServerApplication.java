424413bce0134bcfc7b6dabe06470a3366c7d68c
0woodev <0woodev@gmail.com>
2022-03-16 10:37:33 +0900

    test: 유저 생성시 타임존 설정 되는지 확인

--- src/main/java/com/dev/woo/toyproject/login/loginserver/LoginServerApplication.java
+++ src/main/java/com/dev/woo/toyproject/login/loginserver/LoginServerApplication.java
@@ -3,11 +3,21 @@ package com.dev.woo.toyproject.login.loginserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 // import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableJpaAuditing
 @SpringBootApplication // (exclude = {DataSourceAutoConfiguration.class})
 public class LoginServerApplication {
+
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC+9"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LoginServerApplication.class, args);
 	}
