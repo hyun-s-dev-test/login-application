5bbf7318eb3229b6c658b48780dee0adb1842fac
0woodev <0woodev@gmail.com>
2022-03-16 11:01:35 +0900

    feat: term domain 생성

---
+++ src/main/java/com/dev/woo/toyproject/login/loginserver/domain/term/TermRepository.java
@@ -0,0 +1,6 @@
+package com.dev.woo.toyproject.login.loginserver.domain.term;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermRepository extends JpaRepository<Term, Long> {
+}
